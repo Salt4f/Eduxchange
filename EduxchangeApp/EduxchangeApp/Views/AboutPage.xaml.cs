f6fe105f2852acd82d6ef12ead6ec107cab8eb73
kigrup <raul.plesa@gmail.com>
2021-05-15 16:53:33 +0200

    Changed to Xamarin Forms

---
+++ EduxchangeApp/EduxchangeApp/Views/AboutPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EduxchangeApp.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ EduxchangeApp/EduxchangeApp/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using EduxchangeApp.Models;
+using EduxchangeApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EduxchangeApp.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
