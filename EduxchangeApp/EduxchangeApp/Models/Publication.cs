bf7e1cc566f3b89b5cd3b9814315e22409c49761
xavo7 <xavier14@hotmail.es>
2021-05-15 17:17:17 +0200

    Added models

---
+++ EduxchangeApp/EduxchangeApp/Models/Publication.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EduxchangeApp.Models
+{
+    public enum Tags
+    {
+
+    }
+    public class Publication
+    {
+        public long Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime DateCreated { get; set; } = DateTime.UtcNow;
+
+        public DateTime Deadline { get; set; }
+
+        public byte[] Photo { get; set; }
+
+        public Tags[] Tags { get; set; }
+
+        public bool Fulfilled { get; set; }
+    }
+}
